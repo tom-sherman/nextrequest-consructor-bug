09cd010da8e56ca830abbe041d9e1152bb92d2c6
Tom Sherman <the.tomsherman@gmail.com>
2023-07-20 23:10:12 +0100

    Fix typo

--- app/route/route.ts
+++ app/route/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const dynamic = "force dynamic";
+export const dynamic = "force-dynamic";
 
 // Expected: Echo the request headers
 // Actual: Echos nothing (headers are lost)
