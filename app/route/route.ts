bb0c2ce9ccb9631b2b4d657014cb81bc9b12ca43
Tom Sherman <the.tomsherman@gmail.com>
2023-07-20 22:57:36 +0100

    Better reproduction

--- app/route/route.ts
+++ app/route/route.ts
@@ -2,7 +2,16 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force dynamic";
 
+// Expected: Echo the request headers
+// Actual: Echos nothing (headers are lost)
 export function GET(request: NextRequest) {
   const cloned = new NextRequest(request);
   return NextResponse.json(Object.fromEntries(cloned.headers.entries()));
+}
+
+// Expected: Echo the request body
+// Actual: Empty body
+export async function POST(request: NextRequest) {
+  const cloned = new NextRequest(request);
+  return new NextResponse(await cloned.text());
 }
